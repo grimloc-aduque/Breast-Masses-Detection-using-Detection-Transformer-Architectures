b0400a0129f6213ee4e09c9c03adc2622603652b
grimloc-aduque <aduquead13@gmail.com>
2023-09-11 09:45:00 -0500

    Fixes to run in server

--- YOLOv8/yolo_config.py
+++ YOLOv8/yolo_config.py
@@ -10,6 +10,6 @@ class Config:
     MODEL_SIZES = ['yolov8n.pt', 'yolov8s.pt', 'yolov8m.pt']
     THRESHOLDS = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     BATCH_SIZE = 16
-    EPOCHS = 100
+    EPOCHS = 200
     YAML_FILE = 'data.yaml'
     # YAML_FILE = 'data_docker.yaml'
