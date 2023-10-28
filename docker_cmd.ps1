aa36db8b8e4f8cfd29f236533d98d1a636879802
gcv-aduque <aduque@greencheckverified.com>
2023-10-28 13:21:18 -0500

    Ultralytics project created

--- docker_cmd.ps1
+++ docker_cmd.ps1
@@ -3,26 +3,26 @@ $command=$args[0]
 $model=$args[1]
 $version=$args[2]
 
-if ($model -eq "yolo"){
-    $project_root = "YOLOv8"
-}else{
+if ($model -eq "detr"){
     $project_root = "DETR"
+}else{
+    $project_root = "ULTRALYTICS"
 }
 
 
 if($command -eq "build"){
-    docker build -t grimloc13/$model-detection:$version -f ./$project_root/docker_build.dockerfile .
+    docker build -t grimloc13/$model:$version -f ./$project_root/docker_build.dockerfile .
 }
 if($command -eq "rebuild"){
-    docker build -t grimloc13/$model-detection:$version -f ./$project_root/docker_rebuild.dockerfile .
+    docker build -t grimloc13/$model:$version -f ./$project_root/docker_rebuild.dockerfile .
 }
 if($command -eq "run"){
-    docker run -d --name $model-$version -it grimloc13/$model-detection:$version /bin/bash
+    docker run -d --name $model-$version -it grimloc13/$model:$version /bin/bash
     docker start $model-$version
     docker exec -it $model-$version /bin/bash
 }
 if($command -eq "run-gpu"){
-    docker run -d --name $model-$version --gpus all -it grimloc13/$model-detection:$version /bin/bash
+    docker run -d --name $model-$version --gpus all -it grimloc13/$model:$version /bin/bash
     docker start $model-$version
     docker exec -it $model-$version /bin/bash
 }
@@ -31,10 +31,10 @@ if($command -eq "execute"){
     docker exec -it $model-$version /bin/bash
 }
 if($command -eq "push"){
-    docker push grimloc13/$model-detection:$version
+    docker push grimloc13/$model:$version
 }
 if($command -eq "pull"){
-    docker pull grimloc13/$model-detection:$version
+    docker pull grimloc13/$model:$version
 }
 if($command -eq "remove"){
     try{
